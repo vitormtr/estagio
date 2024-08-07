8d9a5d688a2f01320cffc598e2282c6d333b348c
vitormtr <vitor.triches@gmail.com>
2024-08-07 18:08:55 -0300

    teste

--- estagio-project/src/components/Form.vue
+++ estagio-project/src/components/Form.vue
@@ -24,7 +24,7 @@
 .form {
   max-width: 400px;
   margin: 0 auto;
-  padding: 20px;
+  padding: 24px;
   background-color: #f9f9f9;
   border-radius: 8px;
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
